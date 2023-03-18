d239d7a064a22a84f7a95d051405f2c0fedb3a85
Shivam Zinzuwadiya <szinzuwadiya3@gmail.com>
2023-03-18 23:08:50 +0530

    modified user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router  = express.Router();
-const {register,login,get_current_user} = require("../controllers/userController");
 
+const {register,login,get_current_user} = require("../controllers/userController");
+const validateToken = require("../middleware/validatetoken");
+
+router.post("/me",validateToken,get_current_user);
 router.route("/login").post(login);
 router.route("/register").post(register);
-router.route("/me").post(get_current_user);
-module.exports = router;
+module.exports = router; 
