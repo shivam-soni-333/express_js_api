88c41007767547f790a3406bb33e2371ea0d2c61
Shivam Zinzuwadiya <szinzuwadiya3@gmail.com>
2023-03-18 23:09:32 +0530

    modified accessiblity of routes (securing routes)

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const validateToken = require("../middleware/validatetoken");
+
 const router = express.Router();
 const {getProduct,getProductDetailById,createProduct,updateProduct,deleteProduct} = require("../controllers/productController");
 
 router.route("/").get(getProduct).post(createProduct);
-router.route("/:id").get(getProductDetailById).put(updateProduct).delete(deleteProduct);
+router.route("/:id").get(validateToken, getProductDetailById).put(validateToken,updateProduct).delete(validateToken,deleteProduct);
 
 
 module.exports= router;
